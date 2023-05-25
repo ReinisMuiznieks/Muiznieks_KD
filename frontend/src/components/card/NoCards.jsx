408d38132c1f08f3a7969e89884ea17b8098c7dc
ReinisMuiznieks <muizreinis@gmail.com>
2023-05-25 16:09:15 +0300

    Added_navbar_to_nocards

--- frontend/src/components/card/NoCards.jsx
+++ frontend/src/components/card/NoCards.jsx
@@ -3,20 +3,22 @@ import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import image from '../../images/no_cards.svg'
 import Button from 'react-bootstrap/Button';
+import NavbarTop from "../navbar/Navbar";
 
 function NoCards() {
 
     return (
 <>
+<NavbarTop/>
   <div id="learn-legend">
-    <Container>
-        <Stack id="notfound-stack">
-            <img src={image} alt="404" id="notfound-image"></img>
-            <h3 id="notfound-title" className="text-center">No cards!</h3>
-            <Button variant="outline-secondary" id="notfound-button" href="/learn">Back</Button>
-        </Stack>
-    </Container>
-</div>
+        <Container>
+            <Stack id="notfound-stack">
+                <img src={image} alt="404" id="notfound-image"></img>
+                <h3 id="notfound-title" className="text-center">No cards!</h3>
+                <Button variant="outline-secondary" id="notfound-button" href="/learn">Back</Button>
+            </Stack>
+        </Container>
+    </div>
 
 </>
     )
